1eaf8f8d9377a6b9eadb0b0bbd17f7fb90563ca9
christian.tzolov@gmail.com <christian.tzolov@gmail.com@5ddef761-7f46-07fa-d379-bf5d8de96c69>
2012-01-15 14:06:02 +0000

    [maven-release-plugin]  copy for tag net.tzolov.roo.addon.oauth2-0.4.0

---
+++ tags/net.tzolov.roo.addon.oauth2-0.4.0/src/main/resources/net/tzolov/roo/addon/oauth2/provider/TemplateProtectedResourceController-template.java
@@ -0,0 +1,26 @@
+package __TOP_LEVEL_PACKAGE__.web;
+
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class __PROTECTED_RESOURCE_CONTROLLER__Controller {
+	
+	@RequestMapping(value= "/__PROTECTED_RESOURCE_MAPPING__")
+	public void getResourceData(Model uiModel, HttpServletResponse response) throws Exception {
+
+		response.setContentType("application/json;charset=UTF-8");
+		
+		PrintWriter writer = response.getWriter();
+		
+		writer.write("{\"question\":\"What is the Ultimate Answer to the Ultimate Question of Life, The Universe, and Everything\", " +
+				"\"answer\":\"42\"}");
+		writer.flush();
+	}
+}
