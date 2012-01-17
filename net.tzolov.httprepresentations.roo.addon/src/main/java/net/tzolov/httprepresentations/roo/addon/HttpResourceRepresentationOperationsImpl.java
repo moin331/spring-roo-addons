c7ece2899ca5f1019d049b41defbd93581a66838
christian.tzolov@gmail.com <christian.tzolov@gmail.com@5ddef761-7f46-07fa-d379-bf5d8de96c69>
2012-01-17 09:35:17 +0000

    - Upgrade to Spring Roo 1.2 (not Roo 1.1 compatible anymore) - Upgrade to Spring 3.1

--- net.tzolov.httprepresentations.roo.addon/src/main/java/net/tzolov/httprepresentations/roo/addon/HttpResourceRepresentationOperationsImpl.java
+++ net.tzolov.httprepresentations.roo.addon/src/main/java/net/tzolov/httprepresentations/roo/addon/HttpResourceRepresentationOperationsImpl.java
@@ -1,13 +1,14 @@
 package net.tzolov.httprepresentations.roo.addon;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 import java.util.logging.Logger;
 
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
-import org.springframework.roo.classpath.PhysicalTypeMetadataProvider;
+import org.springframework.roo.classpath.TypeLocationService;
 import org.springframework.roo.metadata.MetadataService;
 import org.springframework.roo.model.JavaType;
 import org.springframework.roo.process.manager.FileManager;
@@ -20,7 +21,7 @@ import org.springframework.roo.project.ProjectOperations;
 import org.springframework.roo.support.logging.HandlerUtils;
 import org.springframework.roo.support.util.Assert;
 import org.springframework.roo.support.util.FileCopyUtils;
-import org.springframework.roo.support.util.TemplateUtils;
+import org.springframework.roo.support.util.FileUtils;
 import org.springframework.roo.support.util.XmlUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -46,19 +47,19 @@ public class HttpResourceRepresentationOperationsImpl implements HttpResourceRep
 	@Reference private PathResolver pathResolver;
 	@Reference private MetadataService metadataService;
 	@Reference private ProjectOperations projectOperations;
-	@Reference private PhysicalTypeMetadataProvider physicalTypeMetadataProvider;
-
+	@Reference private TypeLocationService typeLocationService;
+	   
 	public boolean isInstallHttpResourceRepresentationAvailable() {
 
 		ProjectMetadata project = (ProjectMetadata) metadataService
-				.get(ProjectMetadata.getProjectIdentifier());
+				.get(ProjectMetadata.getProjectIdentifier(projectOperations.getFocusedModuleName()));
 
 		if (project == null) {
 			return false;
 		}
 
 		// only permit installation if not applied so far
-		if (fileManager.exists(pathResolver.getIdentifier(
+		if (fileManager.exists(pathResolver.getFocusedIdentifier(
 				Path.SPRING_CONFIG_ROOT,
 				APPLICATION_CONTEXT_CONTENTRESOLVER_XML))) {
 
@@ -66,7 +67,7 @@ public class HttpResourceRepresentationOperationsImpl implements HttpResourceRep
 		}
 
 		// only permit installation if they don't already have some version of
-		return project
+		return project.getPom()
 				.getDependenciesExcludingVersion(DEPENDENCY_JACKSON_JAXRS)
 				.size() == 0;
 	}
@@ -77,35 +78,34 @@ public class HttpResourceRepresentationOperationsImpl implements HttpResourceRep
 		Element configuration = XmlUtils.getConfiguration(getClass());
 
 		// Add dependencies to POM
-		updateDependencies(configuration);
+		updateDependencies(configuration, this.projectOperations.getFocusedModuleName());
 
 		// copy the template across
-		String destination = pathResolver.getIdentifier(
-				Path.SPRING_CONFIG_ROOT,
-				APPLICATION_CONTEXT_CONTENTRESOLVER_XML);
+		String destination = pathResolver.getFocusedIdentifier(Path.SPRING_CONFIG_ROOT, APPLICATION_CONTEXT_CONTENTRESOLVER_XML);
 
 		if (!fileManager.exists(destination)) {
 			try {
-				FileCopyUtils.copy(TemplateUtils.getTemplate(getClass(),
-						"applicationContext-contentresolver-template.xml"),
-						fileManager.createFile(destination).getOutputStream());
+				String input = FileCopyUtils.copyToString(new InputStreamReader(FileUtils.getInputStream(getClass(), "applicationContext-contentresolver-template.xml")));				
+
+				FileCopyUtils.copy(input.getBytes(), fileManager.createFile(destination).getOutputStream());
 			} catch (IOException ioe) {
 				throw new IllegalStateException(ioe);
 			}
 		}
 	}
 
+
 	public boolean isInstallContentOxmBinding() {
 
 		ProjectMetadata project = (ProjectMetadata) metadataService
-				.get(ProjectMetadata.getProjectIdentifier());
+				.get(ProjectMetadata.getProjectIdentifier(this.projectOperations.getFocusedModuleName()));
 
 		if (project == null) {
 			return false;
 		}
 
 		// only permit if the HttpResourceRepresentation is already setup
-		return fileManager.exists(pathResolver.getIdentifier(
+		return fileManager.exists(pathResolver.getFocusedIdentifier(
 				Path.SPRING_CONFIG_ROOT,
 				APPLICATION_CONTEXT_CONTENTRESOLVER_XML));
 	}
@@ -113,14 +113,15 @@ public class HttpResourceRepresentationOperationsImpl implements HttpResourceRep
 	public void installContentOxmBinding(JavaType typeName) {
 		Assert.notNull(typeName, "Java type required");
 
-		String id = physicalTypeMetadataProvider.findIdentifier(typeName);
+		
+		String id = typeLocationService.getPhysicalTypeIdentifier(typeName);
 		if (id == null) {
 			logger.warning("Cannot locate source for '"
 					+ typeName.getFullyQualifiedTypeName() + "'");
 			return;
 		}
 
-		String contentresolver = pathResolver.getIdentifier(
+		String contentresolver = pathResolver.getFocusedIdentifier(
 				Path.SPRING_CONFIG_ROOT,
 				APPLICATION_CONTEXT_CONTENTRESOLVER_XML);
 
@@ -155,13 +156,13 @@ public class HttpResourceRepresentationOperationsImpl implements HttpResourceRep
 		XmlUtils.writeXml(mutableFile.getOutputStream(), document);
 	}
 	
-	private void updateDependencies(Element configuration) {
+	private void updateDependencies(Element configuration, final String moduleName) {
 		List<Element> databaseDependencies = XmlUtils.findElements(
 				"/configuration/httprepresentations/dependencies/dependency", configuration);
 
 		for (Element dependencyElement : databaseDependencies) {
 			projectOperations
-					.addDependency(new Dependency(dependencyElement));
+					.addDependency(moduleName, new Dependency(dependencyElement));
 		}
 	}
 
